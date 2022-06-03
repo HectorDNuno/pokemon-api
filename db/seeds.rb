e302a82f7b727956893a95573c0debefb593949d
hector <hectornuno1994@gmail.com>
2022-06-03 11:12:09 -0700

    adds water type to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 Type.create(name: "electric", image_url: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg", pokemon_with_type: 77, moves_with_type: 45)
 Type.create(name: "fairy", image_url: "https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg", pokemon_with_type: 68, moves_with_type: 32)
 Type.create(name: "fighting", image_url: "https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg", pokemon_with_type: 80, moves_with_type: 53)
@@ -19,4 +12,4 @@ Type.create(name: "poison", image_url: "https://upload.wikimedia.org/wikipedia/c
 Type.create(name: "psychic", image_url: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg", pokemon_with_type: 124, moves_with_type: 75)
 Type.create(name: "rock", image_url: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg", pokemon_with_type: 80, moves_with_type: 24)
 Type.create(name: "steel", image_url: "https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg", pokemon_with_type: 75, moves_with_type: 34)
-Type.create(name: "steel", image_url: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg", pokemon_with_type: 159, moves_with_type: 47)
+Type.create(name: "water", image_url: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg", pokemon_with_type: 159, moves_with_type: 47)
